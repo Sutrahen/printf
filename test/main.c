d8f5b2875c8282e63dbbe363d26ba298e2de9117
Sutrahen <emmanuelurieto@gmail.com>
2022-10-17 08:26:51 -0700

    printf testing

---
+++ test/main.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * A function that returns the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+		printf("%s/n", argv[0]);
+	return (0);
+}
